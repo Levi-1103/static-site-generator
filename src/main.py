5fbfc8bd3b1618d9ffca4d3aece338a1def91193
Levi <58707841+Levi-1103@users.noreply.github.com>
2024-09-29 22:33:10 +0100

    removed public folder

--- src/main.py
+++ src/main.py
@@ -5,12 +5,33 @@ def main():
     source_dir = "static"
     dest_dir = "public"
 
+   
+
+    if os.path.exists(dest_dir):
+        shutil.rmtree(dest_dir)
+    os.mkdir(dest_dir)
+
     copy_files(source_dir, dest_dir)
 
     
 
 
 def copy_files(source_dir, dest_dir):
+
+    paths = os.listdir(source_dir)
+
+    for path in paths:
+        combined_path = os.path.join(source_dir, path)
+        if os.path.isfile(combined_path):
+            shutil.copy(combined_path, dest_dir)
+            # print("File" + combined_path)
+        else:
+            copy_files(combined_path, dest_dir)
+
+        
+        
+
+def copy_files_using_shutil(source_dir, dest_dir):
     if os.path.exists(dest_dir):
         shutil.rmtree(dest_dir)
 
