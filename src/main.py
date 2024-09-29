56d0ed5f71a25244439d66fda9a324954ab3dd03
Levi <58707841+Levi-1103@users.noreply.github.com>
2024-09-29 22:08:40 +0100

    Added function to copy files from static dir to public dir

--- src/main.py
+++ src/main.py
@@ -1,33 +1,20 @@
-from markdown_blocks import markdown_to_blocks, markdown_to_html_node
-from inline_markdown import (
-    extract_markdown_images,
-    split_nodes_delimiter,
-    split_nodes_image,
-    split_nodes_link,
-    text_to_textnodes,
-)
-from textnode import TextNode, text_node_to_html_node
-from htmlnode import HTMLNode
-
-
-text_type_text = "text"
-text_type_bold = "bold"
-text_type_italic = "italic"
-text_type_code = "code"
-text_type_link = "link"
-text_type_image = "image"
-
+import os
+import shutil
 
 def main():
+    source_dir = "static"
+    dest_dir = "public"
 
-    markdown = """# This is a heading
+    copy_files(source_dir, dest_dir)
 
-This is a paragraph of text. It has some **bold** and *italic* words inside of it.
+    
 
-* This is the first list item in a list block
-* This is a list item
-* This is another list item"""
-    print(markdown_to_html_node(markdown))
+
+def copy_files(source_dir, dest_dir):
+    if os.path.exists(dest_dir):
+        shutil.rmtree(dest_dir)
+
+    shutil.copytree(source_dir, dest_dir)
 
 
 if __name__ == "__main__":
